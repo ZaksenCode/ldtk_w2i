3db1fbdeca0379b6ff917f4c96900d03f1ac7569
ZaksenCode <maksimsidorkin2006@mail.ru>
2025-09-02 15:03:32 +0300

    not sute, but working

--- src/generator.rs
+++ src/generator.rs
@@ -4,11 +4,12 @@ use image::{DynamicImage, GenericImageView, ImageReader, RgbaImage};
 
 use crate::schemas::{LDtk, Level};
 
+#[derive(Debug)]
 struct WorldDims {
   x: i32,
   y: i32,
-  width: i32,
-  height: i32,
+  width: u32,
+  height: u32,
 }
 
 // TODO: Generate image from world levels
@@ -18,12 +19,29 @@ pub fn world_image(world: &LDtk, img_path: &PathBuf) -> RgbaImage {
     world_size.width.try_into().unwrap(),
     world_size.height.try_into().unwrap(),
   );
+
+  let y_less_zero = world_size.y < 0;
+  let x_less_zero = world_size.x < 0;
+
   for level in &world.levels {
     let lvl_img = level_image(level, img_path);
-    for x in (level.world_x as u32)..((level.world_x + level.px_wid) as u32) {
-      for y in (level.world_y as u32)..((level.world_y + level.px_hei) as u32) {
+    println!("{:?}, {:?}", level.world_x, level.world_x + level.px_wid);
+    println!("{:?}, {:?}", level.world_y, level.world_y + level.px_hei);
+
+    for x in 0..lvl_img.width() {
+      for y in 0..lvl_img.height() {
         let pixel = lvl_img.get_pixel(x, y);
-        img.put_pixel(x, y, pixel.to_owned());
+        let ny = if y_less_zero {
+          (-world_size.y + level.world_y) as u32 + y
+        } else {
+          (world_size.y + level.world_y) as u32 + y
+        };
+        let nx = if x_less_zero {
+          (-world_size.x + level.world_x) as u32 + x
+        } else {
+          (world_size.x + level.world_x) as u32 + x
+        };
+          img.put_pixel(nx, ny, pixel.to_owned());
       }
     }
   }
@@ -70,8 +88,20 @@ fn total_world_size(world: &LDtk) -> WorldDims {
       (levela.world_y + levela.px_hei).cmp(&(levelb.world_y + levelb.px_hei))
     })
     .unwrap();
-  dims.width = max_level_x.world_x + max_level_x.px_wid;
-  dims.height = max_level_y.world_y + max_level_y.px_hei;
+  dims.width = (max_level_x.world_x + max_level_x.px_wid) as u32;
+  dims.height = (max_level_y.world_y + max_level_y.px_hei) as u32;
+
+  if dims.x < 0 {
+    dims.width += (-dims.x) as u32;
+  } else {
+    dims.width -= (dims.x) as u32;
+  }
+
+  if dims.y < 0 {
+    dims.height += (-dims.y) as u32;
+  } else {
+    dims.height -= (dims.y) as u32;
+  }
 
   dims
 }
